8968011749a093e7f55c25fd4f1890d60c65b303
OEMV <oscar1_@hotmail.com>
2016-10-21 09:31:01 -0700

    tes

--- app/core/user/user.service.js
+++ app/core/user/user.service.js
@@ -2,6 +2,7 @@
 
 angular
   .module('core.user')
-  .factory('UserService', function ($resource) {
-    return $resource('http://localhost:8080/api/users/:email',{email: "@email"});
-  });
+  .factory('User', ['$resource', function($resource){
+      return $resource('http://localhost:8080/api/users/:email', {email:'@email'}, {});
+  }
+]);
--- app/signup/signup.component.js
+++ app/signup/signup.component.js
@@ -4,23 +4,46 @@ angular
   .module('signUp')
   .component('signUp', {
     templateUrl: 'signUp/signup-form.html',
-    controller: ['$routeParams', '$http', function($routeParams, $http){
+    controller: ['$routeParams', '$http', '$location', function($routeParams, $http, $location){
        var $ctrl = this;
-       var user = {};
+       $ctrl.user = { display_image : ''};
        $ctrl.api_creds = {
            email: 'johndoe@outlook.com',
            password: 'foobar'
-       }
+       };
 
-       $ctrl.doSubmit = function(){
+       $ctrl.signUp = function(){
          $http
          .post('http://localhost:8080/authenticate',$ctrl.api_creds)
          .then(
-            function(response){
-              console.log(response);
+           function(response){
+                  var isSuccess = response.data.success;
+                  if(isSuccess){
+                      var request = {
+                            method: 'POST',
+                            url: 'http://localhost:8080/api/users',
+                            headers:{
+                              'x-access-token': response.data.access_token
+                            },
+                            data: $ctrl.user
+                      };
+
+                      $http(request)
+                      .then(
+                        function(response){
+                            $location.path('/recipes').replace();
+                            console.dir(response);
+                        },
+                        function(error){
+                          console.dir(error);
+
+                      });
+                }else{
+
+                }
             },
-            function(response){
-              console.log(response);
+            function(error){
+                console.log(error);
             }
           );
        };
